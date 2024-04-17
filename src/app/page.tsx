92035a3d85948b00d89ae2382a4200e9491dc1c6
jshkm <2joshmail@gmail.com>
2024-04-17 12:24:50 -0400

    SVG LETS GO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import XSVG from '/x.svg'
+import XSVG from '../../public/x.svg'
 import Image from 'next/image'
 export default function Home() {
   return (
     <div className='flex items-center justify-center h-screen w-screen'>
       <div className='bg-white h-2/3 aspect-square grid min-h-80 grid-cols-3 gap-4 place-content-between'>
-        <Tiles piece={<Image src={'/x.svg'} width={500} height={500} alt='xsvg'></Image>}></Tiles>
+        <Tiles piece={<Image src={XSVG} width={500} height={500} alt='xsvg'></Image>}></Tiles>
         <Tiles piece={'x'}></Tiles>
         <Tiles piece={'x'}></Tiles>
         <Tiles piece={'x'}></Tiles>
