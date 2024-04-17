921a546efb4f776ae17e39d87fc11b9b36da19c9
jshkm <2joshmail@gmail.com>
2024-04-17 12:20:52 -0400

    added tiles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
-import Board from './board.jsx';
-
+import XSVG from '/x.svg'
+import Image from 'next/image'
 export default function Home() {
   return (
-    <>
-      <Board />
-    </>
+    <div className='flex items-center justify-center h-screen w-screen'>
+      <div className='bg-white h-2/3 aspect-square grid min-h-80 grid-cols-3 gap-4 place-content-between'>
+        <Tiles piece={<Image src={'/x.svg'} width={500} height={500} alt='xsvg'></Image>}></Tiles>
+        <Tiles piece={'x'}></Tiles>
+        <Tiles piece={'x'}></Tiles>
+        <Tiles piece={'x'}></Tiles>
+        <Tiles piece={'x'}></Tiles>
+        <Tiles piece={'x'}></Tiles>
+        <Tiles piece={'x'}></Tiles>
+        <Tiles piece={'x'}></Tiles>
+        <Tiles piece={'x'}></Tiles>
+      </div>
+    </div>
+    // <>
+    //   <Image src='/x.svg' width={500} height={500}></Image>
+    // </>
   );
 }
+
+const Tiles = ({ piece }) => (
+  <p className='text-white text-center text-8xl bg-black h-full aspect-square'>
+    {piece}
+  </p>
+);
 
 import React, { useState } from 'react';
