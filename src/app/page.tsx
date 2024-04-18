e900ea2944290065693b4fdacd9536623585d20b
jshkm <2joshmail@gmail.com>
2024-04-18 15:52:24 -0400

    use effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+"use client";
 import X from '../../public/x.svg'
 import O from '../../public/o.svg'
 import Image from 'next/image'
+import React, { useState, useEffect } from 'react'
+
 export default function Home() {
+
+  useEffect(() => {
+    console.log('turn ' + turn)
+  }, [])
+
+  const [turn, setTurn] = useState('X')
+  
+
   return (
     <div className='flex items-center justify-center h-screen w-screen'>
-      <div className='bg-white h-2/3 aspect-square grid min-h-80 grid-cols-3 gap-4'>
+      <div className='bg-white h-2/3 aspect-square grid min-h-80 grid-cols-3 gap-2'>
         <Tiles piece={X}></Tiles>
         <Tiles piece={X}></Tiles>
         <Tiles piece={O}></Tiles>
@@ -24,5 +35,3 @@ const Tiles = ({ piece }) => (
       <Image src={piece} width={500} alt='xsvg'></Image>
     </div>
 );
-
-import React, { useState } from 'react';
