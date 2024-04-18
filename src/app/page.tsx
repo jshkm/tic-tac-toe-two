0d8dd8e05b7831e857725c3f37773ef9ca0fdf9d
jshkm <2joshmail@gmail.com>
2024-04-18 15:09:34 -0400

    tiles sized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,28 @@
-import XSVG from '../../public/x.svg'
+import X from '../../public/x.svg'
+import O from '../../public/o.svg'
 import Image from 'next/image'
 export default function Home() {
   return (
     <div className='flex items-center justify-center h-screen w-screen'>
-      <div className='bg-white h-2/3 aspect-square grid min-h-80 grid-cols-3 gap-4 place-content-between'>
-        <Tiles piece={<Image src={XSVG} width={500} height={500} alt='xsvg'></Image>}></Tiles>
-        <Tiles piece={'x'}></Tiles>
-        <Tiles piece={'x'}></Tiles>
-        <Tiles piece={'x'}></Tiles>
-        <Tiles piece={'x'}></Tiles>
-        <Tiles piece={'x'}></Tiles>
-        <Tiles piece={'x'}></Tiles>
-        <Tiles piece={'x'}></Tiles>
-        <Tiles piece={'x'}></Tiles>
+      <div className='bg-white h-2/3 aspect-square grid min-h-80 grid-cols-3 gap-4'>
+        <Tiles piece={X}></Tiles>
+        <Tiles piece={X}></Tiles>
+        <Tiles piece={O}></Tiles>
+        <Tiles piece={X}></Tiles>
+        <Tiles piece={X}></Tiles>
+        <Tiles piece={X}></Tiles>
+        <Tiles piece={X}></Tiles>
+        <Tiles piece={X}></Tiles>
+        <Tiles piece={X}></Tiles>
       </div>
     </div>
-    // <>
-    //   <Image src='/x.svg' width={500} height={500}></Image>
-    // </>
   );
 }
 
 const Tiles = ({ piece }) => (
-  <p className='text-white text-center text-8xl bg-black h-full aspect-square'>
-    {piece}
-  </p>
+    <div className='flex bg-black h-full aspect-square'>
+      <Image src={piece} width={500} alt='xsvg'></Image>
+    </div>
 );
 
 import React, { useState } from 'react';
