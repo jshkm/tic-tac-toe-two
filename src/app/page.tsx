efde93760d993719659f4464c85d0c5e74d280e7
jshkm <2joshmail@gmail.com>
2024-04-22 20:35:10 -0400

    visibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,36 +11,38 @@ export default function Home() {
   }, [])
 
   const [turn, setTurn] = useState(1)
-  const [visible, setVisible] = useState('invisible')
-  const [board, setBoard] = useState([[null, null, null,], [null, null, null], [null, null, null]])
+  const [board, setBoard] = useState([[X, null, null,], [null, null, null], [null, null, null]])
   
-  
+  const setPiece = () => {
+  console.log('setpiece')
+  setVisible(' visible')
+}
+
+  const [visible, setVisible] = useState(' invisible')
+
+  const Tiles = ({ piece }) => (
+    <div className='flex bg-black h-full aspect-square'>
+      <button onClick={setPiece}>
+        <Image className={'hover:scale-125' + visible}  src={piece} width={500} alt='xsvg'></Image>
+      </button>
+    </div>
+  );
 
   return (
+    <>
     <div className='flex items-center justify-center h-screen w-screen'>
       <div className='bg-white h-2/3 aspect-square grid min-h-80 grid-cols-3 gap-2'>
-        <Tiles piece={X}></Tiles>
-        <Tiles piece={X}></Tiles>
-        <Tiles piece={O}></Tiles>
-        <Tiles piece={X}></Tiles>
-        <Tiles piece={X}></Tiles>
-        <Tiles piece={X}></Tiles>
-        <Tiles piece={X}></Tiles>
-        <Tiles piece={X}></Tiles>
-        <Tiles piece={X}></Tiles>
+        {board.map(b0 => (
+          b0.map(b1 => (
+            <Tiles piece={X}></Tiles>
+            // <div className={'border-2 border-rose-300 ' + visible}></div>
+          ))
+        ))}
       </div>
     </div>
+    {/* <button onClick={() => setVisible('bg-red-500')} className='text-white'>push me</button> */}
+    </>
   );
 }
 
-const setPiece = () => {
-  console.log('setPiece')
-}
 
-const Tiles = ({ piece }) => (
-  <div className='flex bg-black h-full aspect-square'>
-    <button onClick={setPiece}>
-      <Image className={'hover:scale-125'}  src={piece} width={500} alt='xsvg'></Image>
-    </button>
-  </div>
-);
