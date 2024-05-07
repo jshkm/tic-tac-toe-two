076c76da4fea10de32cea8b133375942d57042d4
jshkm <2joshmail@gmail.com>
2024-05-07 14:12:27 -0400

    multiple fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-// import {  } from "next/font/google";
+import { Inter } from "next/font/google";
 import localFont from 'next/font/local'
 import "./globals.css";
 
-// const inter = Inter({ subsets: ["latin"] });
-const myFont = localFont({ src: '../../public/myFont.ttf'})
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter"});
+const myFont = localFont({ src: '../../public/myFont.ttf', variable: "--myFont"})
+
 export const metadata: Metadata = {
   title: "Tic Tac Toe Two",
   description: "The new way to play tic tac toe",
@@ -16,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={myFont.className}>{children}</body>
+    <html lang="en" className={`${myFont.variable} ${inter.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Board from './board.tsx'
+import Board from './board'
 export default function Home() {
+
+  
+
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
-      <h1 className='text-7xl'>Tic Tac Toe 2</h1>
-      <Board></Board>
+      <h1 className='myFont text-7xl'>Tic Tac Toe 2</h1>
+      { true ? (
+        <Board></Board>
+      ) : (
+        <div></div>
+      )}
+      <p>made by josh</p>
     </div>
   );
 }
